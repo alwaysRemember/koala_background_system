82cacd30f053bfa1c36cd313de84939832ab875e
“always” <740905172@qq.com>
2020-12-08 15:31:00 +0800

    fix => 订单列表搜索样式bug step2

--- src/pages/OrderList/index.tsx
+++ src/pages/OrderList/index.tsx
@@ -305,19 +305,17 @@ const OrderList = () => {
             label="订单金额"
             className={styles['amount-slider-wrapper']}
           >
-            <div>
-              <span>金额等级:&nbsp;</span>
-              <Select
-                value={orderAmountSelectType}
-                onChange={value => setOrderAmountSelectType(value)}
-              >
-                {[...Object.values(EOrderAmountSelectType)].map(value => (
-                  <Select.Option key={value} value={value}>
-                    {value}
-                  </Select.Option>
-                ))}
-              </Select>
-            </div>
+            <span>金额等级:&nbsp;</span>
+            <Select
+              value={orderAmountSelectType}
+              onChange={value => setOrderAmountSelectType(value)}
+            >
+              {[...Object.values(EOrderAmountSelectType)].map(value => (
+                <Select.Option key={value} value={value}>
+                  {value}
+                </Select.Option>
+              ))}
+            </Select>
             <Slider
               range
               min={0}
